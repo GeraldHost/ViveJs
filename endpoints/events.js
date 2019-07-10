4ae405b1630a8b26e89145120b6971dfb52e8607
Gerald <me@jacobford.co.uk>
2019-07-10 08:41:27 +0100

    added delete ticket type endpoint

--- endpoints/events.js
+++ endpoints/events.js
@@ -16,10 +16,16 @@ module.exports = function events(http) {
 
     function deleteEvent() { }
 
+    function deleteTicketType(event_id, ticket_type_id) {
+        return http.delete(`events/${event_id}/ticket_types`, {data: [ticket_type_id]})
+            .then((resp) => resp.data);
+    }
+
     return {
         getEvents,
         updateEvent,
         createEvent,
-        deleteEvent
+        deleteEvent,
+        deleteTicketType
     };
 }
